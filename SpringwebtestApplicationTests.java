702a32bad5c3fbf424d3c456882f8b4d8e1e00e1
dazao <@email@dazao.com>
2022-10-27 13:55:23 +0800

    .

---
+++ SpringwebtestApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+    @Test
+    void testmethod(){
+        System.out.println("helloworld");
+    }
+}
+
+
